042d40d303f520a6dfae60083cfb9ddd5d8c86aa
李照飞 <li.zhaofei15@xcar.com.cn>
2016-06-23 10:22:30 +0800

    去除ndk有关的所有代码，把ndk方面的代码单独分离出来了，目前只有一个 卸载监听 的ndk功能，分离到了lib-observeruninstall这个Android Library中，项目地址：https://github.com/stateofzhao/lib-observeruninstall

--- src/main/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-#APP_PLATFORM:=android-10
-#TARGET_CPU_API:= armeabi
-#APP_ABI:= armeabi
-TARGET_CPU_API:=armeabi-v7a
-APP_ABI:=armeabi-v7a
-#APP_STL:=gnustl_static
-#APP_MODULES:=libandroidutil_static
-#APP_CFLAGS+=-Wall -Werror -Wno-long-long -Wno-variadic-macros
