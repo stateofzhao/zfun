dd15e1e166bdb5c0486a57f176dffd12e18bb963
李照飞 <li.zhaofei15@xcar.com.cn>
2016-08-09 12:06:09 +0800

    Clean Architecture实现完成，并且添加了完整示例

---
+++ src/main/java/com/diagramsf/datalayer/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 数据层。
+ */
+package com.diagramsf.datalayer;
---
+++ src/main/java/com/diagramsf/domain/example/GetStudentByIdInteractor.java
@@ -0,0 +1,67 @@
+package com.diagramsf.domain.example;
+
+import com.diagramsf.datalayer.example.StudentDataRepository;
+import com.diagramsf.executor.FireMainThread;
+import com.diagramsf.executor.FireThread;
+import com.diagramsf.executor.InteractorHandler;
+
+/**
+ * Created by Diagrams on 2016/8/9 11:48
+ */
+public class GetStudentByIdInteractor extends BaseInteractor {
+  private static final long WAIT_TIME = 1500;
+
+  private StudentRepository repository;
+  private String id;
+  private FireThread postThread;
+  private StudentRepository.Callback callback;
+
+  public GetStudentByIdInteractor(String id) {
+    repository = new StudentDataRepository();
+    postThread = new FireMainThread();
+    this.id = id;
+  }
+
+  public void execute(Object tag, StudentRepository.Callback callback) {
+    this.callback = callback;
+    InteractorHandler.instance().execute(this, tag);
+  }
+
+  @Override public void run() {
+    waitToDoThisSampleMoreInteresting();
+    try {
+      final StudentEntity studentEntity = repository.getStudentById(id);
+      notifyResponse(studentEntity);
+    } catch (Exception e) {
+      notifyError();
+    }
+  }
+
+  private void waitToDoThisSampleMoreInteresting() {
+    try {
+      Thread.sleep(WAIT_TIME);
+    } catch (InterruptedException e) {
+      //Empty
+    }
+  }
+
+  private void notifyResponse(final StudentEntity studentEntity) {
+    if (!isCancel()) {
+      postThread.post(new Runnable() {
+        @Override public void run() {
+          callback.onResponse(studentEntity);
+        }
+      });
+    }
+  }
+
+  private void notifyError() {
+    if (!isCancel()) {
+      postThread.post(new Runnable() {
+        @Override public void run() {
+          callback.onError();
+        }
+      });
+    }
+  }
+}
