07a34836a911420ee71cb62e9b6a8ae89a24e4f7
李照飞 <li.zhaofei15@xcar.com.cn>
2017-02-16 11:34:10 +0800

    添加Volley关于缓存处理这一块的逻辑注释{@link Cache}

--- src/main/java/com/android/volley/Cache.java
+++ src/main/java/com/android/volley/Cache.java
@@ -20,81 +20,88 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
+ * Volley对于缓存的处理比较特殊，它没有直接借助于网络实现部分的缓存（例如，使用okhttp库自带的缓存功能），
+ * 而是直接实现了自己单独的缓存，并且这个单独实现的缓存还是会自己来读取结果的http报头信息，如果过去还是会重新请求网络的。
+ * <P>
+ *
  * An interface for a cache keyed by a String with a byte array as data.
  */
 public interface Cache {
-    /**
-     * Retrieves an entry from the cache.
-     * @param key Cache key
-     * @return An {@link Entry} or null in the event of a cache miss
-     */
-    public Entry get(String key);
+  /**
+   * Retrieves an entry from the cache.
+   *
+   * @param key Cache key
+   * @return An {@link Entry} or null in the event of a cache miss
+   */
+  public Entry get(String key);
 
-    /**
-     * Adds or replaces an entry to the cache.
-     * @param key Cache key
-     * @param entry Data to store and metadata for cache coherency, TTL, etc.
-     */
-    public void put(String key, Entry entry);
+  /**
+   * Adds or replaces an entry to the cache.
+   *
+   * @param key Cache key
+   * @param entry Data to store and metadata for cache coherency, TTL, etc.
+   */
+  public void put(String key, Entry entry);
 
-    /**
-     * Performs any potentially long-running actions needed to initialize the cache;
-     * will be called from a worker thread.
-     */
-    public void initialize();
+  /**
+   * Performs any potentially long-running actions needed to initialize the cache;
+   * will be called from a worker thread.
+   */
+  public void initialize();
 
-    /**
-     * Invalidates an entry in the cache.
-     * @param key Cache key
-     * @param fullExpire True to fully expire the entry, false to soft expire
-     */
-    public void invalidate(String key, boolean fullExpire);
+  /**
+   * Invalidates an entry in the cache.
+   *
+   * @param key Cache key
+   * @param fullExpire True to fully expire the entry, false to soft expire
+   */
+  public void invalidate(String key, boolean fullExpire);
 
-    /**
-     * Removes an entry from the cache.
-     * @param key Cache key
-     */
-    public void remove(String key);
+  /**
+   * Removes an entry from the cache.
+   *
+   * @param key Cache key
+   */
+  public void remove(String key);
 
-    /**
-     * Empties the cache.
-     */
-    public void clear();
+  /**
+   * Empties the cache.
+   */
+  public void clear();
 
-    /**
-     * 缓存的数据和元数据
-     * 
-     * 
-     * Data and metadata for an entry returned by the cache.
-     */
-    public static class Entry {
-        /** The data returned from cache. */
-        public byte[] data;
+  /**
+   * 缓存的数据和元数据
+   *
+   *
+   * Data and metadata for an entry returned by the cache.
+   */
+  public static class Entry {
+    /** The data returned from cache. */
+    public byte[] data;
 
-        /** ETag for cache coherency. */
-        public String etag;
+    /** ETag for cache coherency. */
+    public String etag;
 
-        /** Date of this response as reported by the server. */
-        public long serverDate;
+    /** Date of this response as reported by the server. */
+    public long serverDate;
 
-        /** TTL for this record. */
-        public long ttl;
+    /** TTL for this record. */
+    public long ttl;
 
-        /** Soft TTL for this record. */
-        public long softTtl;
+    /** Soft TTL for this record. */
+    public long softTtl;
 
-        /** Immutable response headers as received from server; must be non-null. */
-        public Map<String, String> responseHeaders = Collections.emptyMap();
+    /** Immutable response headers as received from server; must be non-null. */
+    public Map<String, String> responseHeaders = Collections.emptyMap();
 
-        /** True if the entry is expired. */
-        public boolean isExpired() {
-            return this.ttl < System.currentTimeMillis();
-        }
-
-        /** True if a refresh is needed from the original data source. */
-        public boolean refreshNeeded() {
-            return this.softTtl < System.currentTimeMillis();
-        }
+    /** True if the entry is expired. */
+    public boolean isExpired() {
+      return this.ttl < System.currentTimeMillis();
     }
 
+    /** True if a refresh is needed from the original data source. */
+    public boolean refreshNeeded() {
+      return this.softTtl < System.currentTimeMillis();
+    }
+  }
 }
