24ad538b13b9c773527365907ad7b2d2504601a2
zfun <stateofzhao@gmail.com>
2021-12-24 18:02:50 +0800

    TaskHandler 包注释丰富下

--- zfunLib/src/main/java/com/zfun/lib/executor/package-info.java
+++ zfunLib/src/main/java/com/zfun/lib/executor/package-info.java
@@ -1,4 +1,5 @@
 /**
- * 自己实现的能够执行异步任务{@link com.zfun.lib.executor.Task}的一个线程池。
+ * 自己实现的能够执行异步任务{@link com.zfun.lib.executor.Task}的一个线程池。<br/>
+ * {@link  com.zfun.lib.executor.TaskHandler}是入口。
  */
 package com.zfun.lib.executor;
