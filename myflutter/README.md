4c7d5d2d31843aa9d653762f2c5990f3ada677df
zfun <stateofzhao@gmail.com>
2023-06-12 17:57:38 +0800

    my_flutter -> myFlutter

--- myflutter/README.md
+++ myflutter/README.md
@@ -1,4 +1,4 @@
-# my_flutter
+# myflutter
 
 A new Flutter module project.
 
--- myflutter/test/widget_test.dart
+++ myflutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_flutter/main.dart';
+import 'package:myflutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
